85ab97e87f01d4830c516f3f1f0444c6e0d06bcb
VktYe <v.yehorenko@gmail.com>
2025-03-26 17:13:14 -0300

    refactore code. change console.logs to callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,20 @@
 const needle = require('needle');
-const args = process.argv;
-const breedName = args[2];
 
-needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response) => {
-  if (error) {
-    console.log('Error:', error);
-  } else {
-    const data = response.body;
-    console.log(typeof data);
-    if (data.length > 0) {
-      console.log("Description: ", data[0].description);
+const fetchBreedDescription = function(breedName, callback) {
+  needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response) => {
+    if (error) {
+      callback(error, null);
     } else {
-      console.log("Breed not found.");
+      const data = response.body;
+      if (data.length > 0) {
+        callback(null, data[0].description);
+      } else {
+        callback("Breed not found.");
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = {
+  fetchBreedDescription,
+};
